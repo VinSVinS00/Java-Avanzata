fd9fd695bb723992457e664d6df83c95158da06b
VinSVinS00 <vincevitolo@gmail.com>
2025-03-26 11:19:54 +0100

    modifica generica

--- src/progettojavaavanzata1/Main.java
+++ src/progettojavaavanzata1/Main.java
@@ -28,6 +28,7 @@ public class Main extends Application {
             @Override
             public void handle(ActionEvent event) {
                 System.out.println("Hello World!");
+                System.out.println("mammt");
             }
         });
         
