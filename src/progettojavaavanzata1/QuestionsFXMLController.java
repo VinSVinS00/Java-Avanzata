89fc1701370f53ecc1845fe63dc0cb823da398b2
VinSVinS00 <vincevitolo@gmail.com>
2025-03-26 18:32:00 +0100

    work in progress della logica di implementazioni, creazione dei controller, creazione dei 3 fxml principali

--- src/progettojavaavanzata1/QuestionsFXMLController.java
+++ src/progettojavaavanzata1/QuestionsFXMLController.java
@@ -5,11 +5,17 @@
  */
 package progettojavaavanzata1;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -35,20 +41,41 @@ public class QuestionsFXMLController implements Initializable {
     private Label statusAttuale;
     @FXML
     private Label maxDomande;
+    
+    NumericQuestion question;
+    
+    private static int result;
+    
+    Alert alert = new Alert(Alert.AlertType.ERROR);
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        NumericQuestion question = new NumericQuestion();
+        question = new NumericQuestion();
         question.randomInit();
         operando1.setText(question.getNum1() + "");
         operando2.setText(question.getNum2() + "");
         operazione.setText(question.getOperator());
+        result = question.getResult();
         
     }    
 
     @FXML
     private void nextQuestion(ActionEvent event) {
-        
+        if(Integer.parseInt(txfRisposta.getText()) == result){
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("EsitoFXML.fxml"));
+                Parent root = loader.load();
+                EsitoFXMLController esitoController = loader.getController();
+                esitoController.addAnswer(new NumericQuestionAttempt(question,Integer.parseInt(txfRisposta.getText())));
+            } catch (IOException ex) {
+                Logger.getLogger(QuestionsFXMLController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+        }else{
+            
+            alert.setTitle("Alert");
+            alert.setHeaderText("Risposta errata");
+        }
     }
     
 }
